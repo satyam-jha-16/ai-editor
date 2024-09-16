1506abe165c8be6b4427290977d52c9bb4087fb4
Satyam <satyamjha1607@gmail.com>
2024-09-16 09:55:30 +0530

    minor fixes

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -11,22 +11,12 @@ import loadingAnimation from "@/public/animations/loading.json"
 export default function Home() {
   const router = useRouter();
   const { user, isLoading } = useKindeBrowserClient(); // Added isLoading
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-  useEffect(() => {
-    if (!isLoading) {
-      if (!user) {
-        router.push("/api/auth/login");
-      } else {
-        setIsAuthenticated(true);
-      }
-    }
-  }, [user, isLoading, router]);
-
-  if (isLoading || !isAuthenticated) {
+  if (isLoading) {
     return <Lottie animationData={loadingAnimation} loop={true} />;
   }
-
+  if (!user) {
+    router.push("/api/auth/login");
+  }
   const userId = user?.id;
 
   return (
